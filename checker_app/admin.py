bde9d86418a72e4a530c597b7c90606dd8d426ba
Alisher Matkurbanov <alishermatkurbanov0408@gmail.com>
2020-02-20 23:43:30 +0500

    add admin model

--- checker_app/admin.py
+++ checker_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from checker_app.models import URL
+
+
+class URLAdmin(admin.ModelAdmin):
+    readonly_fields = ("response_status",)
+    fields = ("text", "response_status", "check_interval", "is_paused")
+
+
+admin.site.register(URL, URLAdmin)
